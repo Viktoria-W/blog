a490efb9fbd78a461d1f5fc91d36bc3ad235a9ab
Victoria <vicktorianedopekina@yandex.ru>
2022-04-28 15:31:21 +0400

    changed header

--- src/js/app.js
+++ src/js/app.js
@@ -1,41 +1,27 @@
+'use strict'
+
+//импортируем функцию проверки на изображения webp
 import * as flsFunctions from "./modules/functions.js";
 
+//импортируем функцию переключения выбранного меню на главной старнице
+import { menuSelected } from "./modules/menu-selected.js";
+
+//импортируем функцию работы бургера
+import { burger } from "./modules/burger.js";
+
+
+//вызов функци проверки на изобр webp
 flsFunctions.isWebp();
 
+//вызов функци основного меню навигации
+menuSelected();
 
-//Находим кнопку бургер,меню и тело страницы
-const burger = document.querySelector('.header__burger');
-const menu = document.querySelector('.nav');
-const body = document.querySelector('body');
+//вызов функци меню бургера
+burger();
 
 
-burger.addEventListener('click', () => {
-    //При клике блокируем прокрутку страницы
-    body.classList.toggle('lock');
-    //меняем бургер на крестик
-    if (burger.classList.contains('burger')) {
-        burger.classList.remove('burger');
-        burger.classList.add('burger-open');
-    } else {
-        burger.classList.remove('burger-open');
-        burger.classList.add('burger');
-    }
-    //открываем меню 
-    if (menu.classList.contains('nav')) {
-        menu.classList.remove('nav');
-        menu.classList.add('nav__open');
-    } else {
-        menu.classList.add('nav');
-        menu.classList.remove('nav__open');
-    }
-});
 
-//находим выбранное меню на index
-const menuSelected = document.querySelector('.main__nav-link-selected');
-const mainMenu = document.querySelector('.main__nav-list');
-//при нажатии убираем класс selected
-mainMenu.addEventListener('keydown', () => {
-    if (menuSelected.classList.contains('main__nav-link-selected')) {
-        menuSelected.classList.toggle('main__nav-link-selected');
-    }
-});
+
+
+
+
